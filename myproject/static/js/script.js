c3e3c8022bc253b4b960944b9ed295a584368774
yuchi <y.kamata0426@gmail.com>
2023-10-19 21:36:59 +0900

    jsファイル：リファクタリング

--- myproject/static/js/script.js
+++ myproject/static/js/script.js
@@ -1,17 +1,21 @@
-document.addEventListener('DOMContentLoaded', function () { 
+//クライアント側のJavaScriptが実行されているブラウザとサーバーとのWebSocket接続を確立
+var socket = io.connect('http://' + document.domain + ':' + location.port); 
 
-    var socket = io.connect('http://' + document.domain + ':' + location.port); //クライアント側のJavaScriptが実行されているブラウザとサーバーとのWebSocket接続を確立
-    
-    document.querySelector('#sendbtn').onclick = function () {   // 送信ボタンがクリックされたときの処理
-        var message = document.querySelector('#sendmsg').value;
-        socket.emit('message', message);
-        document.querySelector('#sendmsg').value = '';
-    };
+// 変数定義
+var sendBtnElement = document.getElementById("sendbtn")
+var sendMsgElement = document.getElementById("sendmsg")
+var postMsgElement = document.getElementById("messages")
 
-    socket.on('message', function (message) {  // サーバーから送信されたメッセージを受信、ブラウザへの表示を行う
-        var ul = document.getElementById('messages');  
-        var li = document.createElement('li');
-        li.appendChild(document.createTextNode(message));
-        ul.appendChild(li);
-    });
-});
+// sendbtnクリックイベント
+sendBtnElement.addEventListener('click', function(){
+    // クライアントからサーバに送信
+    socket.emit("message", sendMsgElement.value)
+    sendMsgElement.value = ""
+})
+
+// サーバからクライアントに送信
+socket.on("message", function(message){
+    var listMsgElement = document.createElement("li")
+    listMsgElement.appendChild(document.createTextNode(message));
+    postMsgElement.appendChild(listMsgElement);
+})

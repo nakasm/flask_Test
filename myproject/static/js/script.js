75a14def016c155f66248c53a3dadbd36401edbf
yuchi <y.kamata0426@gmail.com>
2023-10-26 20:50:49 +0900

    sendmsgチェンジイベント追加

--- myproject/static/js/script.js
+++ myproject/static/js/script.js
@@ -6,19 +6,33 @@ var sendBtnElement = document.getElementById("sendbtn")
 var sendMsgElement = document.getElementById("sendmsg")
 var postMsgElement = document.getElementById("messages")
 
+// ボタンの無効化
+sendBtnElement.disabled = true
+
+// sendmsgチェンジイベント
+sendMsgElement.addEventListener('change', function(){
+    if(sendMsgElement.value === ""){
+        sendBtnElement.disabled = true
+    }
+    else{
+        sendBtnElement.disabled = false
+    }
+})
+
 // sendbtnクリックイベント
 sendBtnElement.addEventListener('click', function(){
      // ユーザーが送信ボタンをクリックすると上記のイベントリスナーが呼び出される
      // クライアントからサーバーに "message" イベントが送信される
     socket.emit("message", sendMsgElement.value)
     sendMsgElement.value = ""
+    sendBtnElement.disabled =  true
 })
 
 // サーバーから受信した "message" に対するクライアント側の処理
 // クライアント側のJavaScriptは、socket.on('message', function(message)) 
 // を使用し、サーバーからの "message" を待機
 socket.on("message", function(message){
-    var listMsgElement = document.createElement("p")
-    listMsgElement.appendChild(document.createTextNode(message));
-    postMsgElement.appendChild(listMsgElement);
+    var pMsgElement = document.createElement("p")
+    pMsgElement.appendChild(document.createTextNode(message));
+    postMsgElement.appendChild(pMsgElement);
 })

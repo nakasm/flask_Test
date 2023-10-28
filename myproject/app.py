9daa968423772bb7094879f94212a1825b13cfb1
nakasm <nakashimakento767@gmail.dom>
2023-10-29 06:59:33 +0900

    add chatbot's response

--- myproject/app.py
+++ myproject/app.py
@@ -4,22 +4,31 @@ from flask_socketio import SocketIO, emit
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+# チャットボットの関数
+def chat_bot_response(message):
+    try:
+        int_message = int(message)
+        response = f'"{message}" は整数型'
+    except ValueError:
+        try:
+            float_message = float(message)
+            response = f'"{message}" は浮動小数点数型'
+        except ValueError:
+            response = f'"{message}" は文字列型'
+    return response
+
 # クライアントのブラウザがルートにアクセスするとindex()が呼び出される
 @app.route('/')
 # chat.htmlテンプレートを読み込み、webページを表示
 def index():
     return render_template('chat.html')
 
-# 'message' イベントが発生したときに呼び出される
+# 'message' イベントが発生したときに呼び出される 
 @socketio.on('message')
-def display_message_length(message): # メッセージの文字列長を表示する関数
-    try:
-        #emit関数でクライアントに応答メッセージを送信
-        length = len(message)  
-        emit('message', f'You sent: {message}')
-        emit('message', f'Message Length: {length}')
-    except ValueError as e:
-        emit('message', f'Error: {e}')
+def handle_message(message):
+    #emit関数でクライアントに応答メッセージを送信
+    response = chat_bot_response(message)
+    emit('message', response)
 
 if __name__ == '__main__':
     #デバッグモードでFlaskアプリケーションを起動
